4eb38dc93acccea1359ebbd8ad51714e76b46365
Jorgee-hub <jmontiek217@gmail.com>
2026-05-25 02:24:46 +0000

    index css y java

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import "bootstrap";
-import "./style.css";
+    let who = ['Mi tia', 'Mi primo', 'Mi perro', 'Mi hermano'];
+    let action = ['daño', 'tiró', 'mordió', 'se llevó'];
+    let what = ['mi tarea', 'mi telefono', 'mi libro','mi laptop'];
+    let when = ['antes de clase', 'ayer', 'la semana pasada']
+    
+    let randmWho = Math.floor(Math.random() * who.length);
+    let randmAction = Math.floor(Math.random() * action.length);
+    let randmWhat = Math.floor(Math.random() * what.length);
+    let randmWhen = Math.floor(Math.random() * when.length);
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let excusaFinal = who[randmWho] + " " + action[randmAction]+ " " + what[randmWhat]+ " " + when[randmWhen]+ "."
+    document.querySelector("#excusa").innerHTML = excusaFinal;
